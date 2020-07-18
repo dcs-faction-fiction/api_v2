b9047d5325abd4d85f0fea6f826b8bb275023da6
Raffaele Ragni <raffaele.ragni@gmail.com>
2020-07-18 20:15:51 +0200

    daemon endpoint reports

--- src/main/java/factionfiction/api/v2/daemon/DaemonEndpoints.java
+++ src/main/java/factionfiction/api/v2/daemon/DaemonEndpoints.java
@@ -6,18 +6,84 @@ import io.javalin.Javalin;
 import static io.javalin.core.security.SecurityUtil.roles;
 import io.javalin.http.Context;
 import io.javalin.plugin.openapi.annotations.OpenApi;
+import io.javalin.plugin.openapi.annotations.OpenApiContent;
+import io.javalin.plugin.openapi.annotations.OpenApiParam;
+import io.javalin.plugin.openapi.annotations.OpenApiRequestBody;
+import io.javalin.plugin.openapi.annotations.OpenApiResponse;
+import java.util.Arrays;
 import java.util.Map;
+import static java.util.Optional.empty;
+import java.util.UUID;
+import static java.util.stream.Collectors.toList;
 
 public class DaemonEndpoints implements Endpoint {
+
+  final DaemonRepository repository;
+
+  public DaemonEndpoints(DaemonRepository repository) {
+    this.repository = repository;
+  }
 
   @Override
   public void register(Javalin javalin) {
     javalin.get("/v2/daemon-api", this, roles(DAEMON));
+    javalin.post("/v2/daemon-api/servers/:server/warehouse-changed", this::warehouseChanged, roles(DAEMON));
+    javalin.post("/v2/daemon-api/servers/:server/units-moved", this::movedUnits, roles(DAEMON));
+    javalin.post("/v2/daemon-api/servers/:server/units-destroyed", this::destroyedUnits, roles(DAEMON));
   }
 
   @OpenApi(ignore = true)
   @Override
   public void handle(Context ctx) throws Exception {
     ctx.json(Map.of("version", "2"));
+  }
+
+  @OpenApi(
+    description = "Daemon reserved: submits spent ammo from mission",
+    pathParams = {
+      @OpenApiParam(name = "serverid", type = String.class)
+    },
+    requestBody = @OpenApiRequestBody(
+      content = @OpenApiContent(from = WarehousesSpent.class)),
+    responses = {@OpenApiResponse(status = "201")})
+  public void warehouseChanged(Context ctx) {
+    var serverId = ctx.pathParam("server", String.class).get();
+    var spent = ctx.bodyAsClass(ImmutableWarehousesSpent.class);
+
+    repository.reportWarehouses(serverId, spent);
+
+    ctx.result("{}");
+    ctx.status(201);
+  }
+
+  @OpenApi(
+    description = "Receives dead units from the mission",
+    requestBody = @OpenApiRequestBody(
+      content = @OpenApiContent(from = FactionUnitPosition.class, isArray = true)),
+    responses = {@OpenApiResponse(status = "201")}
+  )
+  public void movedUnits(Context ctx) {
+    var moved = Arrays.asList(ctx.bodyAsClass(ImmutableFactionUnitPosition[].class));
+
+    repository.reportMovedUnits(moved);
+
+    ctx.result("{}");
+    ctx.status(201);
+  }
+
+  @OpenApi(
+    description = "Receives dead units from the mission",
+    requestBody = @OpenApiRequestBody(
+      content = @OpenApiContent(from = String.class, isArray = true)),
+    responses = {@OpenApiResponse(status = "201")}
+  )
+  public void destroyedUnits(Context ctx) {
+    var deadUnits = ctx.bodyAsClass(String[].class);
+    var uuids = Arrays.asList(deadUnits).stream().map(UUID::fromString).collect(toList());
+
+    repository.reportDeadUnits(uuids);
+
+    ctx.result("{}");
+    ctx.status(201);
   }
 }
--- src/test/java/factionfiction/api/v2/daemon/DaemonEndpointTest.java
+++ src/test/java/factionfiction/api/v2/daemon/DaemonEndpointTest.java
@@ -1,15 +1,20 @@
 package factionfiction.api.v2.daemon;
 
+import base.game.ImmutableLocation;
 import static factionfiction.api.v2.auth.Roles.DAEMON;
 import io.javalin.Javalin;
 import static io.javalin.core.security.SecurityUtil.roles;
+import io.javalin.core.validation.Validator;
 import io.javalin.http.Context;
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
@@ -18,6 +23,7 @@ public class DaemonEndpointTest {
   UUID user;
   Context ctx;
   Javalin javalin;
+  DaemonRepository repository;
   DaemonEndpoints endpoints;
 
   @BeforeEach
@@ -25,7 +31,8 @@ public class DaemonEndpointTest {
     user = UUID.randomUUID();
     ctx = mock(Context.class);
     javalin = mock(Javalin.class);
-    endpoints = new DaemonEndpoints();
+    repository = mock(DaemonRepository.class);
+    endpoints = new DaemonEndpoints(repository);
   }
 
   @Test
@@ -33,6 +40,9 @@ public class DaemonEndpointTest {
     endpoints.register(javalin);
 
     verify(javalin).get(eq("/v2/daemon-api"), any(), eq(roles(DAEMON)));
+    verify(javalin).post(eq("/v2/daemon-api/servers/:server/warehouse-changed"), any(), eq(roles(DAEMON)));
+    verify(javalin).post(eq("/v2/daemon-api/servers/:server/units-moved"), any(), eq(roles(DAEMON)));
+    verify(javalin).post(eq("/v2/daemon-api/servers/:server/units-destroyed"), any(), eq(roles(DAEMON)));
   }
 
   @Test
@@ -40,5 +50,60 @@ public class DaemonEndpointTest {
     endpoints.handle(ctx);
 
     verify(ctx).json(Map.of("version", "2"));
+  }
+
+  @Test
+  public void testWarehouses() {
+    var request = sampleWarehouseSpent();
+    given(ctx.pathParam("server", String.class))
+      .willReturn(Validator.create(String.class, "server1"));
+    given(ctx.bodyAsClass(ImmutableWarehousesSpent.class))
+      .willReturn(request);
+
+    endpoints.warehouseChanged(ctx);
+
+    verify(repository).reportWarehouses("server1", request);
+  }
+
+  @Test
+  public void testDeadUnits() {
+    var uuid = UUID.randomUUID();
+    var request = new String[]{ uuid.toString() };
+    given(ctx.bodyAsClass(String[].class))
+      .willReturn(request);
+
+    endpoints.destroyedUnits(ctx);
+
+    verify(repository).reportDeadUnits(List.of(uuid));
+  }
+
+  @Test
+  public void testMovedUnits() {
+    var item = sampleMovedUnit();
+    var request = new ImmutableFactionUnitPosition[] { item };
+    given(ctx.bodyAsClass(ImmutableFactionUnitPosition[].class))
+      .willReturn(request);
+
+    endpoints.movedUnits(ctx);
+
+    verify(repository).reportMovedUnits(List.of(item));
+  }
+
+  static ImmutableFactionUnitPosition sampleMovedUnit() {
+    return ImmutableFactionUnitPosition.builder()
+      .id(UUID.randomUUID())
+      .location(ImmutableLocation.builder()
+        .latitude(BigDecimal.ONE)
+        .longitude(BigDecimal.ONE)
+        .altitude(BigDecimal.ONE)
+        .angle(BigDecimal.ONE)
+        .build())
+      .build();
+  }
+
+  static ImmutableWarehousesSpent sampleWarehouseSpent() {
+    return ImmutableWarehousesSpent.builder()
+      .data(List.of())
+      .build();
   }
 }
